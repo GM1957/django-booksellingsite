2eaaff3b0b6e6608395a5f8ff717bbc3970eac1e
GM1957 <tanmoybhowmik03@gmail.com>
2020-07-14 22:10:45 +0530

    yo

--- booksellsite/settings.py
+++ booksellsite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'af7^)tlqwxe^3%s1q%*leke=vbg_4py_5(9#@yh%t$dut)do&7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
